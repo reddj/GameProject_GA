764d217924b34126afe0a5b0c72e67c9876671b5
Johnathan <reddjohnathan@gmail.com>
2019-01-08 16:31:38 -0600

    Got the X to win.

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-let turnFlag = true;
+let move = true;
+let winner = [];
+let x = -1;
+let o = 1;
 for (let i = 1; i <= 9; i++) {
 	$(`.game-square${i}`).click(function() {
-		if (turnFlag) {
+		if (move) {
 			$(`.game-square${i}`).append(
 				'<img class ="x" src="./images/X.png">'
 			);
+			winner[parseInt($(`.game-square${i}`).attr('id'))] = x;
 			$(`.game-square${i}`).off();
-			turnFlag = false;
+			move = false;
 		} else {
 			$(`.game-square${i}`).append(
 				'<img class ="o" src="./images/O.png">'
 			);
+			winner[parseInt($(`.game-square${i}`).attr('id'))] = o;
 			$(`.game-square${i}`).off();
-			turnFlag = true;
+			move = true;
 		}
+		console.log(winner);
+		setTimeout(function() {
+			if (winner[0] === -1 && winner[1] === -1 && winner[2] === -1) {
+				alert('X wins!');
+			}
+			if (winner[3] === -1 && winner[4] === -1 && winner[5] === -1) {
+				alert('X wins!');
+			}
+			if (winner[6] === -1 && winner[7] === -1 && winner[8] === -1) {
+				alert('X wins!');
+			}
+			if (winner[0] === -1 && winner[3] === -1 && winner[6] === -1) {
+				alert('X wins!');
+			}
+			if (winner[1] === -1 && winner[4] === -1 && winner[7] === -1) {
+				alert('X wins!');
+			}
+			if (winner[2] === -1 && winner[5] === -1 && winner[8] === -1) {
+				alert('X wins!');
+			}
+			if (winner[0] === -1 && winner[4] === -1 && winner[8] === -1) {
+				alert('X wins!');
+			}
+			if (winner[2] === -1 && winner[4] === -1 && winner[6] === -1) {
+				alert('X wins!');
+			}
+		}, 100);
 	});
 }
+
