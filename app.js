2754da668e22001a214564def3d4c3d75bb28142
Johnathan <reddjohnathan@gmail.com>
2019-01-09 15:54:54 -0600

    X and O will now win. Working on Play Again button.

--- app.js
+++ app.js
@@ -22,30 +22,87 @@ for (let i = 1; i <= 9; i++) {
 		console.log(winner);
 		setTimeout(function() {
 			if (winner[0] === -1 && winner[1] === -1 && winner[2] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[3] === -1 && winner[4] === -1 && winner[5] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[6] === -1 && winner[7] === -1 && winner[8] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[0] === -1 && winner[3] === -1 && winner[6] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[1] === -1 && winner[4] === -1 && winner[7] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[2] === -1 && winner[5] === -1 && winner[8] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[0] === -1 && winner[4] === -1 && winner[8] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
 			}
 			if (winner[2] === -1 && winner[4] === -1 && winner[6] === -1) {
-				alert('X wins!');
+				alert(
+					' X wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[0] === 1 && winner[1] === 1 && winner[2] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[3] === 1 && winner[4] === 1 && winner[5] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[6] === 1 && winner[7] === 1 && winner[8] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[0] === 1 && winner[3] === 1 && winner[6] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[1] === 1 && winner[4] === 1 && winner[7] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[2] === 1 && winner[5] === 1 && winner[8] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner[0] === 1 && winner[4] === 1 && winner[8] === 1) {
+				alert(
+					' O wins! \n Hit the Play Again button in the top left corner, to play again!'
+				);
+			}
+			if (winner.length === 9 && !winner.includes(undefined)) {
+				alert("It's a draw!");
 			}
 		}, 100);
 	});
 }
 
+$('.reset').click(function() {
+	$('.game-square').empty();
+});
